dcc92391330323be03d7d4d87998f654f64973f3
Ali Raza <56601536+alirazadeveloper@users.noreply.github.com>
2021-06-08 15:24:06 +0500

    Create React Object destructor.js

---
+++ React Object destructor.js
@@ -0,0 +1,10 @@
+let obj={
+	morn:'Morning',
+	aftrnon:'Good afternoon',
+	night:'good night',
+	thank:'ok'
+};
+// alert(obj.aftrnon);
+let {morn,thank} = obj;
+morn = 'Good Morning';
+console.log(morn,'_',thank);
