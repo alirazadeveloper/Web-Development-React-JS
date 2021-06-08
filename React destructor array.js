78f43a6e5f8a6996642d74244dc396e8925e562c
Ali Raza <56601536+alirazadeveloper@users.noreply.github.com>
2021-06-08 15:25:47 +0500

    Create React destructor array.js

---
+++ React destructor array.js
@@ -0,0 +1,13 @@
+ destructor array
+
+let arr =['ali','faizan','ejaz']
+let [ali]=arr;
+console.log(ali);
+
+let arr =['ali','faizan','ejaz']
+let [,faiz]=arr;
+console.log(faiz);
+
+let arr =['ali','faizan','ejaz']
+let [,,ej]=arr;
+console.log(ej);
